d036894c3fafd60f2eeb4bd500b10af81638c91e
kielkow <matheuskiel@fiorifer.com.br>
2019-06-06 14:22:33 -0300

    Add limpar()

--- javascript-avancado-i-aula/negociacao/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula/negociacao/client/js/app/controllers/NegociacaoController.js
@@ -27,5 +27,50 @@ class NegociacaoController {
         )
 
         console.log(negocicao)
+        
+        this.limpar()
+
+        /* EXERCICIO1 --> retornar dobro dos números impares do vetor
+        -- Minha versão --
+        let numeros = [3, 2, 11, 20, 8, 7]
+        let num = numeros.map((item) => {
+            if (item % 2 === 0) {
+                return item
+            }
+            return item * 2
+        })
+        console.log(num)
+        console.log(numeros)
+
+        -- versão do instrutor --
+        let numeros = [3, 2, 11, 20, 8, 7];
+        let novosNumeros = numeros.map(item => item % 2 ? item * 2 : item);
+        console.log(novosNumeros)
+        */
+
+
+        /* EXERCICIO2 --> retorna soma de dois números no vetor
+         function soma(num1, num2){
+             return num1 + num2
+         }
+ 
+         let num = [10, 30]
+ 
+         console.log(soma(...num))
+         */
+
+
+        /*EXERCICIO3 --> código funcional e enxuto
+        var aprovados = avaliacoes.filter(prova => prova.nota >= 7).map(prova => prova.aluno.nome)
+        console.log(aprovados)
+        */
+    }
+
+    limpar(){
+        let $ = document.querySelector.bind(document)
+        $('#data').value = ''
+        $('#quantidade').value = ''
+        $('#valor').value = ''
+        $('#data').focus()
     }
 }
