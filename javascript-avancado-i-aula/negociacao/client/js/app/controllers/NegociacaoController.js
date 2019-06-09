4d59a5fc794918ba5f255c55de5d6fe83d8a0bb4
kielkow <matheuskiel@fiorifer.com.br>
2019-06-09 20:13:02 -0300

    Add class ListaNegociacoes and  add method criarNegociacao

--- javascript-avancado-i-aula/negociacao/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula/negociacao/client/js/app/controllers/NegociacaoController.js
@@ -6,65 +6,63 @@ class NegociacaoController {
         this._inputData = $("#data")
         this._inputQuantidade = $("#quantidade")
         this._inputValor = $("#valor")
+        this._listaNegociacoes = new ListaNegociacoes()
 
     }
 
     adiciona(event) {
         event.preventDefault()
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._limpar()
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        )
-
-        console.log(negociacao)
-        console.log(DateHelper.dataParaTexto(negociacao.data))
-
-
-        this.limpar()
-
-        /* EXERCICIO1 --> retornar dobro dos números impares do vetor
-        -- Minha versão --
-        let numeros = [3, 2, 11, 20, 8, 7]
-        let num = numeros.map((item) => {
-            if (item % 2 === 0) {
-                return item
-            }
-            return item * 2
-        })
-        console.log(num)
-        console.log(numeros)
-
-        -- versão do instrutor --
-        let numeros = [3, 2, 11, 20, 8, 7];
-        let novosNumeros = numeros.map(item => item % 2 ? item * 2 : item);
-        console.log(novosNumeros)
-        */
-
-
-        /* EXERCICIO2 --> retorna soma de dois números no vetor
-         function soma(num1, num2){
-             return num1 + num2
-         }
- 
-         let num = [10, 30]
- 
-         console.log(soma(...num))
-         */
-
-
-        /*EXERCICIO3 --> código funcional e enxuto
-        var aprovados = avaliacoes.filter(prova => prova.nota >= 7).map(prova => prova.aluno.nome)
-        console.log(aprovados)
-        */
+            this._inputValor.value)
     }
 
-    limpar() {
-        let $ = document.querySelector.bind(document)
-        $('#data').value = ''
-        $('#quantidade').value = ''
-        $('#valor').value = ''
-        $('#data').focus()
+    _limpar() {
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0.0
+        this._inputData.focus()
     }
+
+    /* EXERCICIO1 --> retornar dobro dos números impares do vetor
+  -- Minha versão --
+  let numeros = [3, 2, 11, 20, 8, 7]
+  let num = numeros.map((item) => {
+      if (item % 2 === 0) {
+          return item
+      }
+      return item * 2
+  })
+  console.log(num)
+  console.log(numeros)
+
+  -- versão do instrutor --
+  let numeros = [3, 2, 11, 20, 8, 7];
+  let novosNumeros = numeros.map(item => item % 2 ? item * 2 : item);
+  console.log(novosNumeros)
+  */
+
+
+    /* EXERCICIO2 --> retorna soma de dois números no vetor
+     function soma(num1, num2){
+         return num1 + num2
+     }
+ 
+     let num = [10, 30]
+ 
+     console.log(soma(...num))
+     */
+
+
+    /*EXERCICIO3 --> código funcional e enxuto
+    var aprovados = avaliacoes.filter(prova => prova.nota >= 7).map(prova => prova.aluno.nome)
+    console.log(aprovados)
+    */
 }
--- javascript-avancado-i-aula/negociacao/client/js/app/helpers/DateHelper.js
+++ javascript-avancado-i-aula/negociacao/client/js/app/helpers/DateHelper.js
@@ -1,13 +1,11 @@
 class DateHelper {
 
-    constructor(){
+    constructor() {
         throw new Error('DateHelper não pode ser instanciada')
     }
 
     static dataParaTexto(data) {
-        return data.getDate() + '/'
-            + (data.getMonth() + 1) + '/'
-            + data.getFullYear()
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
     }
 
 
@@ -18,6 +16,10 @@ class DateHelper {
         let dataTeste = new Date(parseInt(teste[0]), parseInt(teste[1] - 1), parseInt(teste[2]))
         console.log(dataTeste)
         */
+
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Data deve estar no formato aaaa-mm-dd')
+
 
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2))
     }
