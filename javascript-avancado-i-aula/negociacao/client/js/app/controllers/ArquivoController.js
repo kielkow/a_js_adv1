4595c50b327c9335f37daaf73b6e7c0ba37e41eb
kielkow <matheuskiel@fiorifer.com.br>
2019-06-11 22:29:03 -0300

    Add ex Arquivo

--- javascript-avancado-i-aula/negociacao/client/js/app/controllers/ArquivoController.js
+++ javascript-avancado-i-aula/negociacao/client/js/app/controllers/ArquivoController.js
@@ -5,9 +5,23 @@ class ArquivoController {
     }
 
     envia() {
-        //cria um Arquivo com as suas propriedades;
+        /*minha versao
+        let arquivo = new Arquivo(...this._inputDados.value.split('/').map((item) => item.toUpperCase()))
         this._limpaFormulario();
-        // exibe mensagem no console com os dados do arquivo.
+        console.log(arquivo)
+        */
+
+        /*versao instrutor
+        let dados = this._inputDados.value.toUpperCase().split('/');
+        let arquivo2 = new Arquivo(...dados); // usando spread operador
+        console.log(`Dados do arquivo: ${arquivo2.nome}, ${arquivo2.tamanho}, ${arquivo2.tipo}`);
+        this._limpaFormulario();
+        */
+
+        //versao com helper
+        let arquivo3 = ArquivoHelper.cria(this._inputDados.value)
+        console.log(`Dados do arquivo: ${arquivo3.nome}, ${arquivo3.tamanho}, ${arquivo3.tipo}`);
+        this._limpaFormulario()
     }
 
     _limpaFormulario() {
